a3cf78de6e561d6ca9430617f310a5423d8cc9fb
Ms Chen <150866920+mschen-sta@users.noreply.github.com>
2024-04-10 16:41:41 -0400

    Initial commit

---
+++ FahrenheitToCelsius.py
@@ -0,0 +1,7 @@
+#-------------------------------------------------------------------------
+# Name:         Fahrenheit to Celsius
+# Purpose:		  Converts the temperature in Fahrenheit to Celsius
+# Author:       Chen. C
+# Created:      22/03/2024
+#-------------------------------------------------------------------------
+
